187e678c45109c6df3cbe2493e10e8ef7b504dd6
Andreas Lennartz <andreas.lennartz@gmail.com>
2020-02-13 20:35:27 +0100

    Adding StarSchema Solution

---
+++ StarSchema/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ALE.StarSchema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
