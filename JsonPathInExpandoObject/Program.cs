8fa6fb97eb845d922a31a1bde5885e6f3aa0bbcf
Andreas Lennartz <mail@etlbox.net>
2023-02-06 23:01:57 +0100

    Adding demo for json path expressoins in dynamic objects

---
+++ JsonPathInExpandoObject/Program.cs
@@ -0,0 +1,35 @@
+﻿using ETLBox.DataFlow;
+using ETLBox.DataFlow.Connectors;
+using ETLBox.DataFlow.Transformations;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Dynamic;
+
+XmlSource source = new XmlSource("demo.xml", ResourceType.File);
+source.XmlReaderSettings.DtdProcessing = System.Xml.DtdProcessing.Ignore;
+source.ElementName = "entry";
+
+RowTransformation row = new RowTransformation();
+row.TransformationFunc = row => {
+    dynamic newRow = new ExpandoObject();
+    var json = JsonConvert.SerializeObject(row);
+    var jobj = JObject.Parse(json);
+    JToken nameToken = jobj.SelectToken("$.set.element[1].node.@name");
+    JToken nodeToken = jobj.SelectToken("$.set.element[1].node.#text");
+    newRow.Name = nameToken.Value<string>();
+    newRow.NodeValue = nodeToken.Value<string>();
+    return newRow;
+
+};
+
+MemoryDestination dest = new MemoryDestination();
+
+source.LinkTo(row);
+row.LinkTo(dest);
+
+Network.Execute(source);
+
+/* Display data */
+
+foreach (dynamic record in dest.Data) 
+    Console.WriteLine($"Name: {record.Name}, Value: {record.NodeValue}");
