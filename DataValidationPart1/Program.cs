6783299915df8c1123b7f2ff9a62ec901b4ba07f
Andreas Lennartz <mail@etlbox.net>
2023-09-27 15:21:38 +0200

    Updating data validation example

---
+++ DataValidationPart1/Program.cs
@@ -0,0 +1,60 @@
+﻿using DataValidationPart1;
+using ETLBox.ControlFlow;
+using ETLBox.Csv;
+using ETLBox.DataFlow;
+using ETLBox.Json;
+using ETLBox.SqlServer;
+
+//Adjust connection string to your local database
+string SqlConnectionString = @"Data Source=localhost;User Id=sa;Password=YourStrong@Passw0rd;Initial Catalog=demo;TrustServerCertificate=true";
+
+//Prepare the target database
+SqlConnectionManager connectionManager = new SqlConnectionManager(SqlConnectionString);
+DropTableTask.DropIfExists(connectionManager, "VendorMaster");
+CreateTableTask.CreateIfNotExists(connectionManager, "VendorMaster",
+    new List<TableColumn>() {
+        new TableColumn() { Name = "Id", DataType = "INT", AllowNulls = false, IsPrimaryKey = true, IsIdentity = true },
+        new TableColumn() { Name = "VendorName", DataType = "VARCHAR(100)", AllowNulls = false },
+        new TableColumn() { Name = "Code", DataType = "CHAR(5)", AllowNulls = false },
+        new TableColumn() { Name = "Custom", DataType = "VARCHAR(10)", AllowNulls = false },
+        new TableColumn() { Name = "Country", DataType = "CHAR(2)", AllowNulls = true },
+        new TableColumn() { Name = "Contact", DataType = "VARCHAR(50)", AllowNulls = true },
+        new TableColumn() { Name = "Info", DataType = "VARCHAR(50)", AllowNulls = true },
+    });
+SqlTask.ExecuteNonQuery(connectionManager,
+    @"INSERT INTO VendorMaster (VendorName, Code, Custom, Country, Contact, Info)
+        VALUES('BIG HOLDING', 'H1234', 'HD', 'US', 'Hans', 'T0')");
+SqlTask.ExecuteNonQuery(connectionManager,
+    @"INSERT INTO VendorMaster (VendorName, Code, Custom,Country) 
+        VALUES('UNICORN', 'UNI10', 'U', 'NO')");
+SqlTask.ExecuteNonQuery(connectionManager,
+    @"INSERT INTO VendorMaster (VendorName, Code, Custom,Country) 
+        VALUES('UNICORN TWO', 'UNI20', 'U', 'SE')");
+
+
+//Create the data flow components
+var source = new CsvSource<VendorMaster>("sourceData.csv");
+var normalize = new RowTransformation<VendorMaster>(row => row.Normalize());
+var errorTarget = new JsonDestination<VendorMaster>("errors.json");
+var dbTarget = new DbDestination<VendorMaster>(connectionManager, "VendorMaster");
+var duplicateCheck = new Distinct<VendorMaster>();
+var lookupExisting = new LookupTransformation<VendorMaster, VendorMasterDbEntry>();
+var lookupSource = new DbSource<VendorMasterDbEntry>(connectionManager, "VendorMaster");
+lookupExisting.Source = lookupSource;
+
+/* Linking the components */
+source.LinkTo(normalize);
+
+normalize.LinkTo(lookupExisting, row => row.IsValid());
+normalize.LinkTo(errorTarget, row => !row.IsValid());
+
+lookupExisting.LinkTo(duplicateCheck, row => !row.IsInDb);
+lookupExisting.LinkTo(errorTarget, row => row.IsInDb);
+
+duplicateCheck.LinkTo(dbTarget);
+duplicateCheck.LinkDuplicatesTo(errorTarget);
+
+//Execute the network
+await Network.ExecuteAsync(source);
+
+Console.WriteLine("Done!");
---
+++ DataValidationPart1/VendorMaster.cs
@@ -0,0 +1,55 @@
+﻿using ETLBox;
+
+namespace DataValidationPart1
+{
+    public class VendorMaster
+    {
+
+        [DbColumnMap("VendorName")]
+        public string Name { get; set; }
+
+        [DistinctColumn]
+        public string Code { get; set; }
+
+        [DistinctColumn]
+        public string Custom { get; set; }
+
+        public string Country { get; set; }
+
+        public string Contact { get; set; }
+
+        [CsvHelper.Configuration.Attributes.Name("TraceInfo")]
+        public string Info { get; set; }
+
+        public int? DbId { get; set; }
+        public bool IsInDb => DbId.HasValue && DbId > 0;
+
+        public VendorMaster Normalize() {
+            this.Name = Name.Trim().ToUpper();
+            this.Contact = Contact.Trim();
+            return this;
+        }
+
+        public bool IsValid() {
+
+            if (string.IsNullOrEmpty(Name) || string.IsNullOrEmpty(Code) || string.IsNullOrEmpty(Custom))
+                return false;
+            if (Name.Length < 5 || Name.Length > 50)
+                return false;
+            if (Code.Length != 5)
+                return false;
+            return true;
+        }
+
+    }
+
+    public class VendorMasterDbEntry
+    {
+        [RetrieveColumn(nameof(VendorMaster.DbId))]
+        public int Id { get; set; }
+        [MatchColumn(nameof(VendorMaster.Code))]
+        public string Code { get; set; }
+        [MatchColumn(nameof(VendorMaster.Custom))]
+        public string Custom { get; set; }
+    }
+}
