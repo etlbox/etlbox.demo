5ec0ccad49fab2f162402e35107b9e0746630363
Andreas Lennartz <mail@etlbox.net>
2025-02-07 10:38:26 +0100

    Updating simple flow demo

---
+++ SimpleFlow/MyRow.cs
@@ -0,0 +1,10 @@
+﻿namespace ETLBoxDemo.SimpeFlow {
+    public class MyRow {
+        [CsvHelper.Configuration.Attributes.Name("rownr")]
+        public string Id { get; set; }
+        public string name { get; set; }
+        public string quantity_m { get; set; }
+        public string quantity_l { get; set; }
+        public string price_in_cents { get; set; }
+    }
+}
--- SimpleFlow/Program.cs
+++ SimpleFlow/Program.cs
@@ -1,24 +1,22 @@
-﻿using ETLBox.Connection;
+﻿using ETLBox;
 using ETLBox.ControlFlow;
-using ETLBox.ControlFlow.Tasks;
-using ETLBox.DataFlow.Connectors;
-using ETLBox.DataFlow.Transformations;
+using ETLBox.Csv;
+using ETLBox.DataFlow;
+using ETLBox.SqlServer;
+using Serilog;
+using Serilog.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Dynamic;
+using System.Threading.Tasks;
 
-namespace ETLBoxDemo.SimpeFlow
-{
-    class Program
-    {
-        static void Preparation()
-        {
-            //Recreate database if it doesn't exist
-            SqlConnectionManager masterConnection = new SqlConnectionManager("Data Source=localhost;User Id=sa;Password=YourStrong@Passw0rd;");
-            DropDatabaseTask.DropIfExists(masterConnection, "demo");
-            CreateDatabaseTask.Create(masterConnection, "demo");
-            SqlConnectionManager dbConnection = new SqlConnectionManager("Data Source=localhost;User Id=sa;Password=YourStrong@Passw0rd;Initial Catalog=demo;");
+namespace ETLBoxDemo.SimpeFlow {
+    class Program {
+        static void Preparation() {
+            SqlConnectionManager dbConnection = new SqlConnectionManager("Data Source=localhost;User Id=sa;Password=YourStrong@Passw0rd;Initial Catalog=demo;TrustServerCertificate=true");
 
             //Create destination table
+            DropTableTask.DropIfExists(dbConnection, "OrderTable");
             CreateTableTask.Create(dbConnection, "OrderTable", new List<TableColumn>()
             {
                 new TableColumn("Id", "INT", allowNulls:false, isPrimaryKey:true, isIdentity:true),
@@ -28,36 +26,78 @@ namespace ETLBoxDemo.SimpeFlow
             });
         }
 
-        static void Main(string[] args)
-        {
-            //Set up database
+        static async Task Main(string[] args) {
+            //Set up database and logging
+            ConnectSerilog();
             Preparation();
 
+            await DataFlow_POCO();
+
+            await DataFlow_Dynamic();
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadLine();
+        }
+
+        private static async Task DataFlow_POCO() {
             //Define the source
-            CsvSource<string[]> source = new CsvSource<string[]>("demodata.csv");
+            CsvSource<MyRow> source = new CsvSource<MyRow>("demodata.csv");
 
             //Define the transformation
-            RowTransformation<string[], Order> rowTrans = new RowTransformation<string[], Order>(
-              row => new Order()
-              {
-                  Item = row[1],
-                  Quantity = int.Parse(row[2]) + int.Parse(row[3]),
-                  Price = int.Parse(row[4]) / 100
-              });
+            RowTransformation<MyRow, Order> rowTrans = new RowTransformation<MyRow, Order>();
+            rowTrans.TransformationFunc =
+              row => new Order() {
+                  Item = row.name,
+                  Quantity = int.Parse(row.quantity_m) + int.Parse(row.quantity_l),
+                  Price = int.Parse(row.price_in_cents) / 100
+              };
 
-            //DbDestination needs a connection manager pointing to the right database
+            //Define the destination            
             SqlConnectionManager connMan = new SqlConnectionManager("Data Source=localhost;User Id=sa;Password=YourStrong@Passw0rd;Initial Catalog=demo;");
-            //Define the destination
             DbDestination<Order> dest = new DbDestination<Order>(connMan, "OrderTable");
 
             //Link & run flow
             source.LinkTo(rowTrans);
             rowTrans.LinkTo(dest);
-            source.Execute();
-            dest.Wait();
+            await Network.ExecuteAsync(source);
+        }
 
-            Console.WriteLine("Press any key to continue...");
-            Console.ReadLine();
+        private static async Task DataFlow_Dynamic() {
+            //Define the source
+            CsvSource source = new CsvSource("demodata.csv");
+
+            //Define the transformation
+            RowTransformation<ExpandoObject, Order> rowTrans = new RowTransformation<ExpandoObject, Order>();
+            rowTrans.TransformationFunc =
+              row => {
+                  dynamic dynamicRow = row as ExpandoObject;
+                  IDictionary<string, object> dictRow = row as IDictionary<string, object>;
+                  Order order = new Order() {
+                      Item = dynamicRow.name,
+                      Quantity = int.Parse(dictRow["quantity_m"].ToString()) + int.Parse(dictRow["quantity_l"].ToString()),
+                      Price = int.Parse(dictRow["price_in_cents"].ToString()) / 100
+                  };
+                  return order;
+              };
+
+            //Define the destination            
+            SqlConnectionManager connMan = new SqlConnectionManager("Data Source=localhost;User Id=sa;Password=YourStrong@Passw0rd;Initial Catalog=demo;");
+            DbDestination<Order> dest = new DbDestination<Order>(connMan, "OrderTable");
+
+            //Link & run flow
+            source.LinkTo(rowTrans);
+            rowTrans.LinkTo(dest);
+            await Network.ExecuteAsync(source);
+        }
+
+        static void ConnectSerilog() {
+            var serilogLogger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .MinimumLevel.Information()
+                .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3} {taskName}] {Message:lj}" + Environment.NewLine)
+                .CreateLogger();
+
+            Settings.LogInstance = new SerilogLoggerFactory(serilogLogger).CreateLogger("Default");
         }
     }
 }
