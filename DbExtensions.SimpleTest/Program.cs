94f83374c5c9468363722ce835acbad64d6e4fb9
Andreas Lennartz <mail@etlbox.net>
2025-04-07 22:37:08 +0200

    Adding sample code for DbExtensions

---
+++ DbExtensions.SimpleTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using ETLBox;
+using ETLBox.ControlFlow;
+using ETLBox.DbExtensions;
+using ETLBox.SqlServer;
+using Microsoft.Data.SqlClient;
+using System.ComponentModel.DataAnnotations;
+
+string connectionString = "Data Source=np:\\\\.\\pipe\\LOCALDB#FDCF2A2E\\tsql\\query;User Id=sa;Password=YourStrong@Passw0rd;Initial Catalog=demo;TrustServerCertificate=true";
+var connection = new SqlConnection(connectionString);
+
+var tableDefinition = TableDefinition.FromCLRType(ConnectionType.SqlServer, typeof(Customer));
+CreateTableTask.CreateIfNotExists(new SqlConnectionManager(connectionString), tableDefinition);
+    
+var customers = Enumerable.Range(1, 4_999)
+    .Select(i => new Customer { Id = i, Name = $"Customer {i}", City = $"City {i % 50}" });
+
+connection.BulkInsert(customers);
+
+public class Customer {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string City { get; set; }
+}
